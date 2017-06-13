c590b320ad6b09bb3013104a23d80f12df002aee
H4x0R <ptlghost@gmail.com>
2017-06-13 12:42:08 +0100

    First implementation of the Player class

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,31 @@
+import objects.units.Mercenary;
 
 public class Player {
 
+    private Mercenary[] units;
+
+    private int gold = 0;
+    private boolean playing;
+
+    public void init() {
+        // TODO: when game starts, players decide on an amount of gold
+        // TODO: after choosing gold amount, buy characters and place them on the field
+        // TODO: choose randomly who will start playing
+    }
+
+    public void changePlayingState() {
+        if(playing) {
+            playing = false;
+        } else {
+            playing = true;
+        }
+    }
+
+    public int getGold() {
+        return this.gold;
+    }
+
+    public boolean isPlaying() {
+        return this.playing;
+    }
 }
