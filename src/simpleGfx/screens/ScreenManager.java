1864bf9f95020d05286508051b3b6fb0e343695c
H4x0R <ptlghost@gmail.com>
2017-06-13 16:39:49 +0100

    Finished ScreenManager

--- src/simpleGfx/screens/ScreenManager.java
+++ src/simpleGfx/screens/ScreenManager.java
@@ -1,37 +1,151 @@
 package simpleGfx.screens;
 
+import sun.font.ScriptRun;
+
+/**
+ * Basic implementation of a ScreenManager
+ * Works like a stack, last screen is the one shown in the screen
+ * If we want the screen before, it just pops the last one and
+ * returns it in case we want to use for something else
+ *
+ * @author João David Silva
+ */
+
 public class ScreenManager {
 
-    private Screen screen = new MenuScreen();
+    private ScreenNode head;
 
-    public void init(Screen screen) {
-        this.screen = screen;
-        this.screen.init();
+    public void push(Screen element) {
+        if(head == null) {
+            head = new ScreenNode(element);
+            return;
+        }
+
+        head.push(element);
     }
 
-    public void start() {
-        update();
-        draw();
-        remove();
+    public Screen pop() {
+        if(head == null) {
+            return null;
+        }
+
+        if(head.getNextNode() == null) {
+            Screen temp = head.getElement();
+            head = null;
+            return temp;
+        }
+
+        return head.pop();
     }
 
-    public void update() {
-        screen.update();
+    public int size() {
+        if(head == null) {
+            return 0;
+        }
+
+        return head.size(1);
     }
 
-    public void draw() {
-        screen.draw();
+    public Screen getElement(int index) {
+        if(head == null) {
+            return null;
+        }
+
+        return head.getElement(index);
     }
 
-    public void remove() {
-        screen.remove();
+    public void remove(int index) {
+        if(head == null) {
+            return;
+        }
+
+        if(index == 0) {
+            if(head.getNextNode() == null) {
+                head = null;
+                return;
+            }
+            head = head.getNextNode();
+            return;
+        }
+
+        head.remove(index);
     }
 
-    public void changeScreen(Screen screen) {
-        init(screen);
-    }
+    private class ScreenNode {
 
-    public void popScreen() {
+        private ScreenNode next;
+        private Screen element;
 
+        public ScreenNode(Screen element) {
+            this.element = element;
+        }
+
+        public void push(Screen element) {
+            if(next == null) {
+                next = new ScreenNode(element);
+                return;
+            }
+
+            next.push(element);
+        }
+
+        public Screen pop() {
+            if(next == null) {
+                return null;
+            }
+
+            if(next.getNextNode() == null) {
+                Screen temp = next.getElement();
+                next = null;
+                return temp;
+            }
+
+            return next.pop();
+        }
+
+        public int size(int before) {
+            if(next == null) {
+                return 1;
+            }
+
+            return next.size(before) + 1;
+        }
+
+        public ScreenNode getNextNode() {
+            return next;
+        }
+
+        public Screen getElement() {
+            return element;
+        }
+
+        public Screen getElement(int index) {
+            if(index < 1) {
+                return element;
+            }
+
+            if(index == 1 && next == null) {
+                return null;
+            }
+
+            return next.getElement(index - 1);
+        }
+
+        public void remove(int index) {
+            if(next == null && index > 0) {
+                return;
+            }
+
+            if(index == 1) {
+                if(next.getNextNode() == null) {
+                    next = null;
+                    return;
+                }
+                next = next.getNextNode();
+                return;
+            }
+
+            next.remove(index - 1);
+        }
     }
 }
