3224d4c9581517805a1b099b6762a39e8f43dfd1
FranciscoCaetano88 <falc1988@gmail.com>
2017-06-14 18:37:22 +0100

    Selection Screen again

--- src/org/academiadecodigo/medievalwar/simpleGfx/screens/SelectionScreen.java
+++ src/org/academiadecodigo/medievalwar/simpleGfx/screens/SelectionScreen.java
@@ -1,7 +1,40 @@
 package org.academiadecodigo.medievalwar.simpleGfx.screens;
 
+import org.academiadecodigo.medievalwar.Player;
+import org.academiadecodigo.medievalwar.objects.units.Mercenary;
+import org.academiadecodigo.simplegraphics.mouse.Mouse;
+import org.academiadecodigo.simplegraphics.mouse.MouseEvent;
+import org.academiadecodigo.simplegraphics.mouse.MouseEventType;
+import org.academiadecodigo.simplegraphics.mouse.MouseHandler;
+
 /**
  * Created by codecadet on 13/06/17.
  */
-public class SelectionScreen {
+public class SelectionScreen implements MouseHandler{
+
+    private Player player;
+    private Mercenary mercenary;
+
+    public SelectionScreen(Player player, Mercenary mercenary) {
+        this.player = player;
+        this.mercenary = mercenary;
+
+        Mouse m = new Mouse(this);
+        m.addEventListener(MouseEventType.MOUSE_CLICKED);
+        m.addEventListener(MouseEventType.MOUSE_MOVED);
+
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
 }
